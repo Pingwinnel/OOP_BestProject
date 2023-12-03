5015ba1b8640b7353020432fcce7660933982200
pokonti <152721937+pokonti@users.noreply.github.com>
2023-12-03 06:48:31 +0600

    Update ResearchFactory.java
    it doesnt work anyway

--- src/wproject/ResearchFactory.java
+++ src/wproject/ResearchFactory.java
@@ -1,15 +1,5 @@
 package wproject;
 
 public class ResearchFactory {
-	public static Researcher makeResearcher(User u) {
-		if(u instanceof Researcher) {
-			Researcher r = (Researcher)u;
-			return r;
-		} else {
-			System.out.println("nono");
-		}
-		return null;
-		
-		
-	}
+	
 }
