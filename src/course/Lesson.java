1fbaba7380b28184eba181340f04b2f46a11e09d
pokonti <pokonti@bk.ru>
2023-12-25 16:20:13 +0600

    upd lesson
    in lesson we will store course

--- src/course/Lesson.java
+++ src/course/Lesson.java
@@ -1,19 +1,27 @@
 package course;
 
+import java.util.Objects;
 
 public class Lesson {
-	String name;
-	Days day;
+	private Course course;
+	private Days day;
 	private Time time;
-	Format format;
-	LessonType type;
+	private Format format;
+	private LessonType type;
 	private Room room;
 	
 	public Lesson() {
 		
 	}
-	public Lesson(String name, Days day, Time time,LessonType type,Room room) {
-		this.name = name;
+	//for test
+	public Lesson(Course course, Days day, Time time, LessonType type) {
+		this.course = course;
+		this.day = day;
+		this.time = time;
+		this.type=type;
+	}
+	public Lesson(Course course, String name, Days day, Time time, LessonType type,Room room) {
+		this.course = course;
 		this.day = day;
 		this.time = time;
 		this.type=type;
@@ -21,14 +29,15 @@ public class Lesson {
 	}
 	
 	public String toString() {
-		if(format == Format.ONLINE) return "Lesson "+ name+ ", "+day + ", "+format + ", "+time + ", " + room + ".";
-		return "Lesson "+ name+ ", "+day + ", "+format + ", "+time + ", " + room + ".";
+		if(format == Format.ONLINE) return "Lesson "+ course.getName()+ ", "+day + ", "+format + ", "+time;
+		return "Lesson "+ course.getName()+ ", "+day + ", "+format + ", "+time + ", " + room + ".";
 	}
-	public String getName() {
-		return name;
+	
+	public Course getCourse() {
+		return course;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setCourse(Course course) {
+		this.course = course;
 	}
 	public Days getDay() {
 		return day;
@@ -60,6 +69,24 @@ public class Lesson {
 	public void setRoom(Room room) {
 		this.room = room;
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(course, day, format, room, time, type);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lesson other = (Lesson) obj;
+		return Objects.equals(course, other.course) && day == other.day && format == other.format
+				&& Objects.equals(room, other.room) && Objects.equals(time, other.time) && type == other.type;
+	}
+	
+	
 
 
 }
