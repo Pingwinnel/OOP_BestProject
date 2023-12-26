ac3f8679c1a7802213591e46c5e9196c94819786
amhalcyon <adinamedelbai@gmail.com>
2023-12-26 18:44:36 +0600

    test manager menu

---
+++ src/test/TestManagerMenu.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import MenuPages.LoginPage;
+import MenuPages.ManagerMenu;
+import course.Course;
+import course.Days;
+import course.Lesson;
+import course.LessonType;
+import course.Time;
+import staff.Dean;
+import staff.Manager;
+import student.Schools;
+import teacher.Teacher;
+import utilities.DataSingleton;
+
+public class TestManegerMenu {
+
+	public static void main(String[] args) throws Exception {
+		
+		Manager m = new Manager();
+		ManagerMenu mm = new ManagerMenu(m);
+		Time t = new Time(13, 0);
+		Course c = new Course("OOP");
+		Teacher tr = new Teacher("ghjk", "hfjk", 34567);
+		Lesson l = new Lesson(c, Days.FRI, LessonType.LAB, t);
+		Lesson l2 = new Lesson(c, Days.MON, LessonType.PRACTICE, t);
+		HashMap<Teacher, Lesson> lessonsOfTeachers = new HashMap<Teacher, Lesson>();
+		lessonsOfTeachers.put(tr, l);
+		lessonsOfTeachers.put(tr, l2);
+		DataSingleton.INSTANCE.addUser(tr);
+		DataSingleton.INSTANCE.addUser(m);
+		//System.out.println(lessonsOfTeachers.get(tr));
+		//System.out.println(tr.getCorparateEmail());
+		//System.out.println(mm.findUserByEmail("h.ghjk@kbtu.kz"));
+		Dean d = new Dean("erty", "ergh", 567890, Schools.SITE);
+		DataSingleton.INSTANCE.addUser(d);
+		//System.out.println(d.getCorparateEmail());
+		//System.out.println(mm.findUserByEmail("e.erty@kbtu.kz"));
+		
+		try { 
+			(new ManagerMenu(m)).run(); 
+		} catch (IOException e) { 
+			e.printStackTrace();
+		}
+		 
+		
+	}
+}
