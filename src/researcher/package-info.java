48e3700e7e800d6d51c91c2e4ab182664165901b
amhalcyon <adinamedelbai@gmail.com>
2023-12-27 01:14:09 +0600

    packages for doc

---
+++ src/researcher/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The researcher package contains classes and interfaces related to researcher and includes classes representing researchers, research papers, and related utilities
+ */
+package researcher;
---
+++ src/student/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The student package contains classes related to students and includes classes representing students, majors, and other student-related entities
+ */
+package student;
