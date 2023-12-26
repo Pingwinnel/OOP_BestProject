cfe04c5a6d7b23ee4aad56337a30b3b30828d6df
Pingwinnel <erasil123gns@gmail.com>
2023-12-26 16:43:13 +0600

    student menu

--- src/MenuPages/studentMenu.java
+++ src/MenuPages/studentMenu.java
@@ -1,6 +1,8 @@
 package MenuPages;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,7 +12,7 @@ import utilities.DataSingleton;
 import utilities.Mark;
 
 public class studentMenu extends UserMenu{
-	Scanner in = new Scanner(System.in);
+	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	Student s;
 	
 	public studentMenu(Student s) {
@@ -35,13 +37,13 @@ public class studentMenu extends UserMenu{
 			e.printStackTrace();
 		}
 	}
-	private void addCourse() {
+	private void addCourse() throws IOException {
 		System.out.println("Enter name of the course: ");
-		DataSingleton.INSTANCE.getCourse().add(new Course(in.next()));
+		DataSingleton.INSTANCE.getCourse().add(new Course(bf.readLine()));
 		System.out.println("Course added! ");		
 	}
-	private void addCourseToStudent(Student s) {
-		int i = in.nextInt()-1;
+	private void addCourseToStudent(Student s) throws NumberFormatException, IOException {
+		int i = Integer.parseInt(bf.readLine())-1;
 		Course c = DataSingleton.INSTANCE.getCourse().get(i);
 		if(!s.getMarks().containsKey(c)) {
 			s.getMarks().put(c, new Mark());
@@ -51,22 +53,39 @@ public class studentMenu extends UserMenu{
 	}
 	public void run() throws IOException {
 		try {
-			System.out.println("Welcome!");
+			welcomeMessage();
 			menu : while(true){
-				System.out.println("What do you want to do? \n 1) Add course  \n 2) Exit");
-				int choice = in.nextInt();
+				System.out.println("What do you want to do? \n 1) Add course \n 2) View transcript \n 3) Change password  \n 4) Exit");
+				int choice = Integer.parseInt(bf.readLine());
 					if (choice==1){
 					addCourse: while(true){
 						addCourse();
 						System.out.println("\n 1) Add another course  \n 2) Return back \n 3) Exit");
-						choice = in.nextInt();
+						choice = Integer.parseInt(bf.readLine());
 						if(choice==1) continue addCourse;
 						if(choice==2) continue menu;
 						if(choice==3) {exit();  break menu;}
 						break;
 					}
 				}
-				else if (choice==2){
+					else if (choice==2){
+						s.viewTranscript();
+						System.out.println("\n 1) Return back \n 2) Exit");
+						transcript : while (true) {
+						choice=Integer.parseInt(bf.readLine());
+						if(choice==1) continue menu;
+						if(choice==2) {exit();  break menu;}
+						break;
+							
+						}
+					}
+					else if (choice==3){
+						System.out.println("Print new passord: ");
+						String pass=bf.readLine();
+						s.changePassword(pass);
+						System.out.println("Password succesfuly changed");
+					}
+				else if (choice==4){
 					exit();
 					break menu;
 				}
--- src/test/testStudentMenu.java
+++ src/test/testStudentMenu.java
@@ -1,16 +1,16 @@
 package test;
 
 import java.io.IOException;
+
 import MenuPages.*;
+import student.Student;
 
 public class testStudentMenu {
 
 	public static void main(String[] args) throws Exception {
-		try {
-			( new studentMenu()).run();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		Student s=new Student("Yerasyil","Omirzhanov");
+		studentMenu stuMenu=new studentMenu(s);
+		stuMenu.run();
 
 	}
 
